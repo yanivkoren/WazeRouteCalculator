0dee81adb5ae5f87575e1c2de76fdc45afdd73d8
yanivkoren <yaniv@korenf.com>
2023-10-02 10:50:12 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -9,18 +9,21 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         from_address = request.form.get('from_address')
+        from_address_name = request.form.get('from_address_name')
         to_addresses = request.form.getlist('to_address_choices')
+        to_address_labels = request.form.get('to_address_labels').split('##')
         
         custom_address = request.form.get('to_address_custom')
         if custom_address:
             to_addresses.append(custom_address)
         
         results = []
-        for to_address in to_addresses:
+        #for to_address in to_addresses:
+        for to_address, to_address_label in zip(to_addresses, to_address_labels):
             region = 'IL'
             route = WazeRouteCalculator.WazeRouteCalculator(from_address, to_address, region)
             route_time, route_distance = route.calc_route_info()
-            result = f'מ{from_address} ל{to_address} משך נסיעה %d דקות, מרחק %d ק"מ.' % (route_time, route_distance)
+            result = f'מ{from_address_name} ל{to_address_label} משך נסיעה %d דקות, מרחק %d ק"מ.' % (route_time, route_distance)
             results.append(result)
 
         result_text = '<br>'.join(results)
